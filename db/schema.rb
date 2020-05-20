f924eacf4c3eec04fc6e60d97db8095db55a03b4
concornsoup <concornsoup@gmail.com>
2020-05-20 12:54:35 +0000

    branch0520

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_042952) do
+ActiveRecord::Schema.define(version: 2020_05_20_084146) do
 
-  create_table "posts", force: :cascade do |t|
+  create_table "post2s", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
